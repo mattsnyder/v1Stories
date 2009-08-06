217c8c3d86834a2142466e0f1187de8a1b843fe7
Matt Snyder <matt.snyder@leandog.com>
2009-08-06 09:57:48 -0400

    updated specs to reflect NOT trimming title

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,9 +2,9 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe ApplicationHelper, "trim_story_title" do
   include ApplicationHelper
-  it "should trim a long title" do
-    trim_story_title("X"*50).should end_with("...") 
-    trim_story_title("X"*50).length.should == 48 
+  it "should NOT trim a long title" do
+    trim_story_title("X"*50).should end_with("X") 
+    trim_story_title("X"*50).length.should == 50 
   end
 end
   
@@ -21,12 +21,12 @@ describe ApplicationHelper, "trim_story_description" do
     trim_story_description(string_with_length_250).should end_with("symbols.")
   end
 
-  it "should trim a long string" do
+  it "should NOT trim a long string" do
   string = <<-LONG_STRING
   If the first symbol listed is chosen, then any or all of the symbols following can be used as well. 
   For example, for Liability, if symbol 7 is chosen, then the user can also choose  8, 9, or 19. The user 
   can choose any one or all of those symbols. More text to make this longer.
   LONG_STRING
-  trim_story_description(string).should end_with("...")
+  trim_story_description(string).should end_with("this longer.")
   end
 end
